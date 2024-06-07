a81e2b963e7dc19431af6d392fd61bac673bdcac
ahmed1amen <ahmed1amen.g@gmail.com>
2024-06-07 23:02:35 +0200

    modify

--- resources/views/app-overview.blade.php
+++ resources/views/app-overview.blade.php
@@ -7,7 +7,8 @@
     <!-- Fonts -->
     <link rel="preconnect" href="https://fonts.bunny.net">
     <link href="https://fonts.bunny.net/css?family=figtree:400,600&display=swap" rel="stylesheet" />
-    @vite(['resources/css/home.css'])
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/bootstrap/5.3.3/css/bootstrap.min.css" integrity="sha512-jnSuA4Ss2PkkikSOLtYs8BlYIeeIK1h99ty4YfvRPAlzr377vr3CXDb7sb7eEEBYjDtcYj+AjBH3FLv5uSJuXg==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/bootstrap/5.3.3/js/bootstrap.min.js" integrity="sha512-ykZ1QQr0Jy/4ZkvKuqWn4iF3lqPZyij9iRv6sGqLRdTPkY69YX6+7wvVGmsdBbiIfN/8OdsI7HABjvEok6ZopQ==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
 </head>
 <body class="bg-gray-100 font-sans bg-image">
     @if(session('success'))
@@ -17,9 +18,21 @@
     </div>
 @endif
 
-    <div class="text-end mt-8">
-        <a href="{{ route('request-company-account') }}" class="bg-gray-500 rounded-xl hover:bg-gray-700 text-white font-bold py-2 px-8">Register
-            here</a>
-    </div>
+   <div class="container">
+       <br>
+   </div>
+   <div class="container">
+
+       <div class="row justify-center text-center">
+           <div class="col-12">
+
+               <a href="{{ route('request-company-account') }}" class="btn-success  btn rounded-xl">Register
+                   here</a>
+
+
+           </div>
+       </div>
+
+   </div>
 </body>
 </html>
